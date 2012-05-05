03d97fcc81980920cca6ed6a28ecc1b71e98e7fa
Paul Phillips <paulp@improving.org>
2012-05-05 00:59:52 -0700

    .

--- src/compiler/scala/tools/nsc/typechecker/Infer.scala
+++ src/compiler/scala/tools/nsc/typechecker/Infer.scala
@@ -1202,6 +1202,46 @@ trait Infer {
         }
       }
     }
+    
+    def abstractTypesInType(tpe: Type) = {
+      
+      
+      for (tp <- tpe ; if tp.typeSymbol.isExistentiallyBound) yield
+        tp.typeSymbol
+    }
+    
+    def typesLostInErasure(tp: Type): List[Type] = {
+      
+      tp.normalize match {
+        case SingleType(pre, _) =>
+          check(pre, bound)
+        case TypeRef(_, ArrayClass, arg :: _) =>
+          check(arg, bound)
+        case tp @ TypeRef(pre, sym, args) =>
+          (  (sym.isAbstractType && !isLocalBinding(sym))
+          || (args exists (x => !isLocalBinding(x.typeSymbol)))
+          || check(pre, bound)
+          )
+        // case RefinedType(_, decls) if decls.nonEmpty =>
+        //   patternWarning(tp, "refinement ")
+        case RefinedType(parents, _) =>
+          parents exists (p => check(p, bound))
+        case ExistentialType(quantified, tp1) =>
+          check(tp1, bound ::: quantified)
+        case _ =>
+          false
+      }
+    }
+    
+    def isSurroundingTypeParam(sym: Symbol) = {
+      val e = context.scope.lookupEntry(sym.name)
+        (    (e ne null)
+          && (e.sym == sym )
+          && !e.sym.isTypeParameterOrSkolem
+          && (e.owner == context.scope)
+        )
+    }
+    
 
     /** Does `tp` contain any types that cannot be checked at run-time (i.e., after erasure, will isInstanceOf[erased(tp)] imply conceptualIsInstanceOf[tp]?)
      * we should find a way to ask erasure: hey, is `tp` going to make it through you with all of its isInstanceOf resolving powers intact?
@@ -1209,14 +1249,6 @@ trait Infer {
      */
     def containsUnchecked(tp: Type): Boolean = {
       def check(tp: Type, bound: List[Symbol]): Boolean = {
-        def isSurroundingTypeParam(sym: Symbol) = {
-          val e = context.scope.lookupEntry(sym.name)
-            (    (e ne null)
-              && (e.sym == sym )
-              && !e.sym.isTypeParameterOrSkolem
-              && (e.owner == context.scope)
-            )
-        }
         def isLocalBinding(sym: Symbol) = (
           sym.isAbstractType && (
                (bound contains sym)
