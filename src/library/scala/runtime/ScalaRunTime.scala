55bf632b3c4d21059dbcbe3119efb58e962e1282
Paul Phillips <paulp@improving.org>
2012-06-04 08:10:57 -0700

    Removed a merge oops.

--- src/library/scala/runtime/ScalaRunTime.scala
+++ src/library/scala/runtime/ScalaRunTime.scala
@@ -340,7 +340,6 @@ object ScalaRunTime {
       case x: AnyRef if isArray(x)      => arrayToString(x)
       case x: collection.Map[_, _]      => x.iterator take maxElements map mapInner mkString (x.stringPrefix + "(", ", ", ")")
       case x: Iterable[_]               => x.iterator take maxElements map inner mkString (x.stringPrefix + "(", ", ", ")")
-      case x: Iterable[_]            => x take maxElements map inner mkString (x.stringPrefix + "(", ", ", ")")
       case x: Product1[_] if isTuple(x) => "(" + inner(x._1) + ",)" // that special trailing comma
       case x: Product if isTuple(x)     => x.productIterator map inner mkString ("(", ",", ")")
       case x                            => x.toString
