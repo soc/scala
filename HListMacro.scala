233f1674eca120c3b63a6f012e8bdd84dfd4f3d9
Paul Phillips <paulp@improving.org>
2012-07-02 02:04:31 -0700

    .

--- HListMacro.scala
+++ HListMacro.scala
@@ -1,33 +1,20 @@
-package scala
+package improving
 
 import scala.reflect.makro.Context
 import language.experimental.macros
+import shapeless._
 
-object HListMacro {
-  def makeHList(args: Any*): HList = macro makeHList_impl
-  def makeHList_impl(c: Context)(args: c.Expr[Any]*) = {
+object HLists {
+  def apply(args: Any*): HList = macro apply_impl
+  def apply_impl(c: Context)(args: c.Expr[Any]*) = {
     import c.universe._
+    import treeBuild._
 
-    val HNilType  = typeOf[scala.HNil.type]
-    val HConsClass = typeOf[scala.HCons[_,_]].typeSymbol
+    def HNilType    = typeOf[shapeless.HNil.type]
+    def HConsModule = typeOf[shapeless.::.type].typeSymbol
+    def trees       = args.toList map (_.tree)
+    def base        = mkAttributedRef(HNilType.typeSymbol.companionSymbol)
 
-    val trees = args.toList map (_.tree)
-    val tpes  = trees map (x => c.typeCheck(x.duplicate).tpe.widen)
-
-    // val hlistType = tpes.foldRight(HNilClass.tpe)((tpe, res) => appliedType(HConsClass, List(tpe, res)))
-    val base = ((treeBuild.mkAttributedRef(HNilType.typeSymbol.companionSymbol), HNilType))
-    val (hlistTree, hlistType) = (trees zip tpes).foldRight(base) { case ((tree, tpe), (rtree, rtpe)) =>
-      (treeBuild.mkMethodCall(HConsClass.companionSymbol, newTermName("apply"), List(tpe, rtpe), List(tree, rtree)),
-        appliedType(typeOf[scala.HCons[_,_]].typeConstructor, List(tpe, rtpe)))
-    }
-
-    c.warning(NoPosition, "" + ((hlistType, hlistType)))
-      //
-      //
-      // trees.foldRight(treeBuild.mkAttributedRef(HNilClass))((tree, res) =>
-      //   treeBuild.mkMethodCall(HConsClass, newTermName("apply"), Nil, List(tree, res))
-      // )
-
-    c.Expr[HList](hlistTree)
+    c.Expr[HList](trees.foldRight(base)((tree, res) => mkMethodCall(HConsModule, newTermName("apply"), Nil, List(tree, res))))
   }
 }
