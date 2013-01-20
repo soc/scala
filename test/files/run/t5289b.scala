d55f78f6c13c37dbb9d1327280d2668617e8a277
Paul Phillips <paulp@improving.org>
2013-01-20 11:16:25 -0800

    good

---
+++ test/files/run/t5289b.scala
@@ -0,0 +1,22 @@
+// package types
+
+trait IStringPair[T] {
+  def a : String
+  def b : String
+  def build(a : String, b : String) : T
+  def cat(that : IStringPair[T]) = build(this.a + that.a, this.b + that.b)
+  override def toString = a + b
+}
+
+class StringPair(val a : String, val b : String) extends IStringPair[StringPair] {
+  def build(a : String, b : String) = new StringPair(a, b)
+  def len = a.length + b.length
+}
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val x = new StringPair("a", "b")
+    val y = x.build("q", "z")
+    println(y.len)
+  }
+}
