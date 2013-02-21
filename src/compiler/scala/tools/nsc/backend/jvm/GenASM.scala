43e850f437383fb4a1e0379b546538a22d939606
Paul Phillips <paulp@improving.org>
2013-02-21 13:36:09 -0800

    genasm

--- src/compiler/scala/tools/nsc/backend/jvm/GenASM.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenASM.scala
@@ -829,13 +829,13 @@ abstract class GenASM extends SubComponent with BytecodeWriters with GenJVMASM {
       if (!needsGenericSignature(sym)) { return null }
 
       // val memberTpe = enteringErasure(sym.info.asSeenFrom(owner.thisType, owner))
-      val memberTpe = enteringErasure(owner.thisType memberInfo sym)
+      def memberTpe = enteringErasure(owner.thisType memberInfo sym)
       // ).asSeenFrom(owner.thisType, owner)
       // val memberTpe1 = enteringErasure(sym.info.asSeenFrom(owner.enclClass.thisType, owner.enclClass))
       // println(s"getGenericSignature($sym, $owner)/memberTpe=$memberTpe")
       // val methodTpe1 = enteringErasure((owner.info memberType sym).asSeenFrom(owner.thisType, owner))
 
-      val jsOpt: Option[String] = erasure.javaSig(sym, memberTpe)
+      val jsOpt: Option[String] = enteringErasure(erasure.javaSig(sym, owner.thisType memberInfo sym))
       if (jsOpt.isEmpty) { return null }
 
       val sig = jsOpt.get
