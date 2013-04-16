6f76224acdc77e327f85127f4c461e274773b0ec
Paul Phillips <paulp@improving.org>
2013-04-16 14:09:12 -0700

    .

---
+++ improving/coll/Frame.java
@@ -0,0 +1,21 @@
+package improving;
+
+public class Frame<T> {
+  private T elem;
+  private long stamp;
+  private int id;
+
+  public T elem() { return elem; }
+  public long stamp() { return stamp; }
+  public int id() { return id; }
+
+  public Frame(T elem, int id) {
+    this.elem = elem;
+    this.id = id;
+    this.stamp = System.nanoTime();
+  }
+
+  @Override public String toString() {
+    return String.format("Frame(elem=%s, id=%s, stamp=%s)", elem, id, new java.util.Date(stamp));
+  }
+}
---
+++ improving/coll/TrackStack.java
@@ -0,0 +1,58 @@
+package improving;
+
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import java.util.concurrent.Callable;
+
+interface TrackStackCallBack<T> {
+  public void done(Frame<T> frame, int currentId);
+}
+
+public class TrackStack<T> {
+  private ArrayDeque<Frame<T>> stack;
+  private int id;
+
+  public TrackStack() {
+    this.stack = new ArrayDeque<Frame<T>>();
+    this.nextid = 1;
+  }
+
+  private int nextId() {
+    try     { return id; }
+    finally { id += 1; }
+  }
+
+  public Frame<T> pop() { return stack.pop().elem(); }
+  public Frame<T> head() { return stack.peek().elem(); }
+  public int depth() { return stack.size(); }
+  public void push(T elem) { stack.push(new Frame(elem, nextId())); }
+
+  public <U> U runWith(T elem) throws Exception {
+    runWith(new TrackStackCallBack<T>() {
+      public void done(Frame<T> frame, int currentDepth, int currentId) {
+        System.err.printf("%s currentDepth=%s currentId=%s\n", frame, currentDepth, currentId);
+      }
+    });
+  }
+
+  public <U> U runWith(T elem, TrackStackCallBack<U> callback) throws Exception {
+    push(elem);
+    try     { return op.call(); }
+    finally { callback.done(stack.pop(), id); }
+  }
+
+  @Override public String toString() {
+    int depth = 0;
+    StringBuilder sb = new StringBuilder();
+    Iterator<Elem> it = stack.descendingIterator();
+    while (it.hasNext()) {
+      T x = it.next().elem();
+      for (int i = 0; i < depth; i++) {
+        sb.append("  ");
+      }
+      sb.append("" + x + "\n");
+      depth++;
+    }
+    return sb.toString();
+  }
+}
