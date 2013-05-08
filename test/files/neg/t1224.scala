9e5be1a97c1f33232b24d51b1a85e6fdf8f041a2
Paul Phillips <paulp@improving.org>
2013-05-08 13:06:29 -0700

    Spruced up test for lower f-bounds.
    It was not exactly testing what it intended, so I fanned
    it out until it couldn't miss.

--- test/files/neg/t1224.scala
+++ test/files/neg/t1224.scala
@@ -1,5 +1,61 @@
-trait C[T] {}
+package q1 {
+  package p1 {
+    trait C[T] {}
 
-abstract class A {
- type T >: C[T] <: C[C[T]]
+    abstract class A1 {
+     type T >: C[T] <: C[T]
+    }
+
+    abstract class A2 {
+     type T >: C[T] <: C[C[T]]
+    }
+
+    abstract class A3 {
+     type T >: C[C[T]] <: C[T]
+    }
+
+    abstract class A4 {
+     type T >: C[C[T]] <: C[C[T]]
+    }
+  }
+
+  package p2 {
+    trait C[-T] {}
+
+    abstract class A1 {
+     type T >: C[T] <: C[T]
+    }
+
+    abstract class A2 {
+     type T >: C[T] <: C[C[T]]
+    }
+
+    abstract class A3 {
+     type T >: C[C[T]] <: C[T]
+    }
+
+    abstract class A4 {
+     type T >: C[C[T]] <: C[C[T]]
+    }
+  }
+
+  package p3 {
+    trait C[+T] {}
+
+    abstract class A1 {
+     type T >: C[T] <: C[T]
+    }
+
+    abstract class A2 {
+     type T >: C[T] <: C[C[T]]
+    }
+
+    abstract class A3 {
+     type T >: C[C[T]] <: C[T]
+    }
+
+    abstract class A4 {
+     type T >: C[C[T]] <: C[C[T]]
+    }
+  }
 }
