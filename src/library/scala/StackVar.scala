19c7d0c1880881b67340b4eb1c48f966ddc36127
Paul Phillips <paulp@improving.org>
2013-05-26 10:25:28 -0700

    ...

---
+++ src/library/scala/StackVar.scala
@@ -0,0 +1,43 @@
+package scala
+
+import scala.collection.{ mutable, immutable, generic }
+
+object StackVar {
+  private val record = mutable.Set[StackVar[_]]()
+  def dump() {
+    try record.toList.map(_.toString).sorted foreach println
+    finally record.clear()
+  }
+
+  scala.sys addShutdownHook dump()
+
+  def apply[T](initialValue: T): StackVar[T] = {
+    val sv = new StackVar[T](initialValue)
+    record += sv
+    sv
+  }
+}
+
+class StackVar[T] private (initialValue: T) {
+  private[this] var values: List[Update] = initialValue :: Nil
+  private implicit def mkUpdate(value: T): Update = Update(value)
+
+  private case class Update(value: T) {
+    val index             = if (values eq null) 0 else values.length
+    val timestamp         = System.currentTimeMillis
+    private def date_s    = new java.util.Date(timestamp)
+    override def toString = s"#$index $date_s $value"
+  }
+  private def head = values.head
+  def index = head.index
+  def set(value: T): this.type = {
+    values ::= value
+    this
+  }
+  def get: T = values.head.value
+  def size = values.size
+  def dump() {
+    values.reverse foreach println
+  }
+  override def toString = s"$get (var #$index)"
+}
