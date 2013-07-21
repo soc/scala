6e21b7365d2b9a91abd37623d92413e2811a51c9
Paul Phillips <paulp@improving.org>
2013-07-20 19:52:30 -0700

    wip

--- src/compiler/scala/tools/nsc/util/CompatClassPath.scala
+++ src/compiler/scala/tools/nsc/util/CompatClassPath.scala
@@ -22,12 +22,18 @@ import improving.paths.{ ClassPath => NewClassPath }
 //   def findBytes(path: PathRep): Bytes = single findBytes path
 // }
 
-class CompatAbstractFile(val rep: PathRep, override val toByteArray: Array[Byte]) extends AbstractFile {
-  // println(s"CompatAbstractFile($rep, ${toByteArray.length} bytes)")
+// , override val toByteArray: Array[Byte]) extends AbstractFile {
+class CompatAbstractFile(val rep: PathRep, bytesFn: => Array[Byte]) extends AbstractFile {
+  // println(s"CompatAbstractFile($rep, _)")
   def name          = rep.filename
   override def path = rep.path
   override def file = rep.file
 
+  override lazy val toByteArray = {
+    val res = bytesFn
+    println(s"CompatAbstractFile($rep, ${res.size} bytes)")
+    res
+  }
   def absolute: AbstractFile  = this
   def container: AbstractFile = this
   def create()                = ???
@@ -44,36 +50,68 @@ class CompatAbstractFile(val rep: PathRep, override val toByteArray: Array[Byte]
 
 // cp: NewClassPath
 // case class PathMap(name: Name.Simple, paths: List[PathRep], children: List[PathMap]) {
-class CompatClassPath(cp: NewClassPath, root: PathMap) extends ClassPath[AbstractFile] {
+class CompatClassPath(cp: NewClassPath) extends ClassPath[AbstractFile] {
+  private val rootPathMap           = cp.buildHierarchy()
+  private val rootView: PackageView = new PackageView(Name.Root, rootPathMap)
+  private val packageViewMap        = mutable.Map[Name.Dotted, PackageView](Name.Root -> rootView)
+
+  val context: ClassPath.ClassPathContext[AbstractFile] = rootView.context
+
+  class PackageView(enclosing: Name.Dotted, root: PathMap) extends ClassPath[AbstractFile] {
+    def nextPackage(name: Name.Simple): PackageView = {
+      val fqname = enclosing dot name
+      packageViewMap.getOrElseUpdate(fqname, new PackageView(fqname, root(name)))
+    }
+    def fqpackage                        = (enclosing dot root.name).chars
+    def name                             = root.name.toString
+    def classes: IndexedSeq[AnyClassRep] = root.pathsToClasses map toClassRep
+    def packages                         = root.packageNames map nextPackage
+    def asURLs                           = Nil
+    def asClasspathString                = ""
+    def sourcepaths                      = Vector.empty
+
+    private def toClassRep(rep: PathRep): AnyClassRep      = ClassRep(Some(toAbstractFile(rep)), None)
+    private def toAbstractFile(rep: PathRep): AbstractFile = new CompatAbstractFile(rep, cp findBytes rep)
+    private def toFileSystemPath(path: String): PathRep    = PathRep(path.replace('.', '/') + ".class")
+
+    object context extends ClassPath.ClassPathContext[AbstractFile] {
+      def newClassPath(dir: AbstractFile): ClassPath[AbstractFile] = nextPackage(dir.name)
+      def toBinaryName(rep: AbstractFile): String                  = rep.name stripSuffix ".class"
+    }
+  }
+
+  def name                             = rootView.name.toString
+  def classes: IndexedSeq[AnyClassRep] = rootView.classes
+  def packages                         = rootView.packages
+
+  // object context extends ClassPath.ClassPathContext[AbstractFile] {
+  //   def newClassPath(dir: AbstractFile): ClassPath[AbstractFile] = packageViewMap.getOrElseUpdate(
+  //    // new CompatClassPath(cp, root(dir.name))
+  //   def toBinaryName(rep: AbstractFile): String                  = rep.name stripSuffix ".class"
+  // }
+
   // println(s"+  $root")
-  def name        = root.name.toString
-  def binaryNames = root.paths filter (_ hasExtension "class") //map (_ stripSuffix ".class") map (_.replace('/', '.'))
-  def classes     = printResult("classes")(binaryNames flatMap toClassRep toVector)
-  def packages    = printResult("packages")(root.children map (c => new CompatClassPath(cp, c)) toVector)
+  // def name        = root.name.toString
+  // def binaryNames = root.paths filter (_ hasExtension "class") //map (_ stripSuffix ".class") map (_.replace('/', '.'))
+  // def classes     = binaryNames flatMap toClassRep toVector
+  // def packages    = root.children map (c => new CompatClassPath(cp, c)) toVector
+  // private def toAbstractFile(rep: PathRep): AbstractFile = new CompatAbstractFile(rep, cp findBytes rep)
+  // private def toFileSystemPath(name: String): PathRep    = PathRep(name.replace('.', '/') + ".class")
 
-  object context extends ClassPath.ClassPathContext[AbstractFile] {
-    def newClassPath(dir: AbstractFile): ClassPath[AbstractFile] = printResult(s"newClassPath($dir)")(new CompatClassPath(cp, root(dir.name)))
-    def toBinaryName(rep: AbstractFile): String                  = printResult(s"toBinaryName($rep)")(rep.name stripSuffix ".class")
-  }
-  private def printResult[T](msg: String)(res: T): T = {
-    // println(s"$msg: $res")
-    res
-  }
-  // def context = DefaultJavaContext
-
-  private def toAbstractFile(rep: PathRep): AbstractFile = cp findBytes rep match {
-    case NoBytes => NoAbstractFile
-    case bytes   => new CompatAbstractFile(rep, bytes)
-  }
-  private def toClassRep(rep: PathRep): Option[AnyClassRep] = toAbstractFile(rep) match {
-    case NoAbstractFile => None
-    case file           => Some(ClassRep(Some(file), None))
-  }
-  override def findClass(name: String): Option[AnyClassRep] = printResult(name)(toClassRep(PathRep(name.replace('.', '/') + ".class")))
+  // cp findBytes rep match {
+  //   case NoBytes => NoAbstractFile
+  //   case bytes   => new CompatAbstractFile(rep, bytes)
+  // }
+  // private def toClassRep(rep: PathRep): Option[AnyClassRep] = Some(ClassRep(Some(toAbstractFile(rep)), None))
+  // override def findClass(name: String): Option[AnyClassRep] = toClassRep(toFileSystemPath(name))
+  // toAbstractFile(rep) match {
+  //   case NoAbstractFile => None
+  //   case file           => Some(ClassRep(Some(file), None))
+  // }
 
   def asURLs = Nil
   def asClasspathString = ""
   def sourcepaths       = Vector()
-  private def packages_s = root.children.map(_.name).mkString(", ")
-  override def toString = s"CompatClassPath($root)"
+  // private def packages_s = root.children.map(_.name).mkString(", ")
+  // override def toString = s"CompatClassPath($root)"
 }
