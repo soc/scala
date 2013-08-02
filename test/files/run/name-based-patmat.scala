cbcfa357d0571d73c321c066dd5a73a074d5bf45
Paul Phillips <paulp@improving.org>
2013-08-02 08:10:45 -0700

    wip

--- test/files/run/name-based-patmat.scala
+++ test/files/run/name-based-patmat.scala
@@ -18,6 +18,36 @@ package p1 {
   }
 }
 
+package p2 {
+  class Triple(val x: Any) {
+    private def s = "" + x
+    def isEmpty = false
+    def get = this
+    def _1 = s
+    def _2 = "2 " + s + "s! A ha ha!"
+    def _3 = "3 " + s + "s! A ha ha!"
+    override def toString = s"Triple(${_1}, ${_2}, ${_3})"
+  }
+
+  object Triple {
+    def unapply(x: Any): Triple = new Triple(x)
+  }
+}
+
+package p3 {
+  case class Foo(x: Int, y: Int, zs: Int*)
+
+  object Bar {
+    def f(x: Foo) = x match {
+      case Foo(5, 10, 15, 20, _*) => 1
+      case Foo(5, 10, 15, _*)     => 2
+      case Foo(5, 10, _*)         => 3
+      case Foo(5, 10)             => 4 // should warn unreachable
+      case _                      => 5
+    }
+  }
+}
+
 object Test {
 
   // def f(x: Any) = x match {
@@ -30,5 +60,15 @@ object Test {
       case p1.Triple(x, y, z) => List(x, y, z) foreach println
       case x                  => println("fail: " + x)
     }
+    "catdog" match {
+      case p2.Triple(x, y, z) => List(x, y, z) foreach println
+      case x                  => println("fail: " + x)
+    }
+
+    println(p3.Bar.f(p3.Foo(5, 10, 15, 20, 25)))
+    println(p3.Bar.f(p3.Foo(5, 10, 15, 20)))
+    println(p3.Bar.f(p3.Foo(5, 10, 15)))
+    println(p3.Bar.f(p3.Foo(5, 10)))
+    // println(p3.Bar.f(p3.Foo(5)))
   }
 }
