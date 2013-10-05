d47db6891af07d0930d92e81cd4ee754daaa9684
Paul Phillips <paulp@improving.org>
2013-10-05 07:32:36 -0700

    ...

--- src/library/scala/PrimitiveCompanion.scala
+++ src/library/scala/PrimitiveCompanion.scala
@@ -26,6 +26,9 @@ trait PrimitiveCompanion extends Specializable {
   type Unboxed
   type Boxed <: AnyRef
 
+  def isNumeric: Boolean
+  def unboxedClass: Class[Unboxed]
+  def boxedClass: Class[Boxed]
   def box(x: Unboxed): Boxed
   def unbox(x: Boxed): Unboxed
   def zero: Unboxed
@@ -49,6 +52,9 @@ object PrimitiveCompanion extends immutable.IndexedSeq[PrimitiveCompanion] {
 object Boolean extends PrimitiveCompanion {
   type Unboxed             = Boolean
   type Boxed               = jl.Boolean
+  def isNumeric            = false
+  def unboxedClass         = classOf[jl.Boolean]
+  def boxedClass           = classOf[Boolean]
   def zero                 = false
   def box(x: Boolean)      = jl.Boolean.valueOf(x)
   def unbox(x: jl.Boolean) = x.booleanValue
@@ -57,15 +63,21 @@ object Boolean extends PrimitiveCompanion {
 object Byte extends PrimitiveCompanion {
   type Unboxed          = Byte
   type Boxed            = jl.Byte
+  def isNumeric         = true
+  def unboxedClass      = classOf[Byte]
+  def boxedClass        = classOf[jl.Byte]
   def zero              = (0: Byte)
   def box(x: Byte)      = jl.Byte.valueOf(x)
   def unbox(x: jl.Byte) = x.byteValue
 }
 
 object Short extends PrimitiveCompanion {
-  type Unboxed          = Short
-  type Boxed            = jl.Short
-  def zero              = (0: Short)
+  type Unboxed           = Short
+  type Boxed             = jl.Short
+  def isNumeric          = true
+  def unboxedClass       = classOf[Short]
+  def boxedClass         = classOf[jl.Short]
+  def zero               = (0: Short)
   def box(x: Short)      = jl.Short.valueOf(x)
   def unbox(x: jl.Short) = x.shortValue
 }
@@ -73,6 +85,9 @@ object Short extends PrimitiveCompanion {
 object Char extends PrimitiveCompanion {
   type Unboxed               = Char
   type Boxed                 = jl.Character
+  def isNumeric              = true
+  def unboxedClass           = classOf[Char]
+  def boxedClass             = classOf[jl.Character]
   def zero                   = (0: Char)
   def box(x: Char)           = jl.Character.valueOf(x)
   def unbox(x: jl.Character) = x.charValue
@@ -81,6 +96,9 @@ object Char extends PrimitiveCompanion {
 object Int extends PrimitiveCompanion {
   type Unboxed             = Int
   type Boxed               = jl.Integer
+  def isNumeric            = true
+  def unboxedClass         = classOf[Int]
+  def boxedClass           = classOf[jl.Integer]
   def zero                 = 0
   def box(x: Int)          = jl.Integer.valueOf(x)
   def unbox(x: jl.Integer) = x.intValue
@@ -89,23 +107,32 @@ object Int extends PrimitiveCompanion {
 object Long extends PrimitiveCompanion {
   type Unboxed          = Long
   type Boxed            = jl.Long
+  def isNumeric         = true
+  def unboxedClass      = classOf[Long]
+  def boxedClass        = classOf[jl.Long]
   def zero              = (0: Long)
   def box(x: Long)      = jl.Long.valueOf(x)
   def unbox(x: jl.Long) = x.longValue
 }
 
 object Float extends PrimitiveCompanion {
-  type Unboxed          = Float
-  type Boxed            = jl.Float
-  def zero              = (0: Float)
+  type Unboxed           = Float
+  type Boxed             = jl.Float
+  def isNumeric          = true
+  def unboxedClass       = classOf[Float]
+  def boxedClass         = classOf[jl.Float]
+  def zero               = (0: Float)
   def box(x: Float)      = jl.Float.valueOf(x)
   def unbox(x: jl.Float) = x.floatValue
 }
 
 object Double extends PrimitiveCompanion {
-  type Unboxed          = Double
-  type Boxed            = jl.Double
-  def zero              = (0: Double)
+  type Unboxed            = Double
+  type Boxed              = jl.Double
+  def isNumeric           = true
+  def unboxedClass        = classOf[Double]
+  def boxedClass          = classOf[jl.Double]
+  def zero                = (0: Double)
   def box(x: Double)      = jl.Double.valueOf(x)
   def unbox(x: jl.Double) = x.doubleValue
 }
